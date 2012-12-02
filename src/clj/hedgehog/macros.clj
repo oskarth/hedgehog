c6726120d339c46e297c1ce075e36287ce5e0dd1
Oskar Thoren <o.thoren@gmail.com>
2012-12-02 15:09:57 -0500

    trying out graph traversal for html elements

--- src/clj/hedgehog/macros.clj
+++ src/clj/hedgehog/macros.clj
@@ -10,3 +10,41 @@
   "defined computed observables"
   [name expr]
   `(def ~name (computed-observable ~expr)))
+
+;; test data
+(def foo-body '
+  [:div#todos
+    [:ul
+      (map todo-element @todos)
+      (when-not (empty? @pending-todo)
+        (todo-element @pending-todo))]
+    [:input#input
+     {:value @pending-todo
+      :bind-value @pending-todo
+       :type "text"
+       :autofocus "true"}]
+   [:input {:value @pending-todo}]
+   [:button "Add"]])
+
+(defn deref? [form]
+  (= "clojure.core/deref" (str form)))
+
+(defn- eval-to-map? [form]
+  (and (fn? form) (map? (form))))
+
+(defn elem-with-attr? [form]
+  (and (vector? form)
+       (keyword? (first form))
+       (or (map? (second form))
+           (eval-to-map? (second form)))))
+
+;; TODO
+(defn bind-val? [form]
+  (and (elem-with-attr? form)
+       (get :bind-value form)))
+
+(defn walky [form]
+  (clojure.walk/postwalk
+   (fn [f]
+     (if (bind-val? f)
+       ["ELEM " f] f)) form))
